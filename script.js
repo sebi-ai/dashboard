555311c9e6c194891beecbc120d827fe4fcd274d
TX 9394 <sebastian.wimmer@whg-student.de>
2026-06-10 15:08:58 +0000

    Started adding the first settings and did some styles.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function showSection(sectionId) {
   document.getElementById("home").style.display = sectionId === "home" ? "block" : "none";
   document.getElementById("dashboard").style.display = sectionId === "dashboard" ? "flex" : "none";
   document.getElementById("settings").style.display = sectionId === "settings" ? "block" : "none";
+  document.getElementById("About-me").style.display = sectionId === "about" ? "block" : "none";
 }
 
 document.getElementById("redirect-start-btn").addEventListener("click", function(e) {
@@ -19,4 +20,15 @@ document.getElementById("settings-btn").addEventListener("click", function(e) {
   showSection("settings");
 });
 
+document.getElementById("about-btn").addEventListener("click", function(e) {
+  e.preventDefault();
+  showSection("about");
+});
+
 showSection("home");
+
+const checkbox = document.getElementById("use-ip-location");
+const textfeld = document.getElementById("location");
+checkbox.addEventListener("change", function() {
+  textfeld.disabled = checkbox.checked;
+});
