c982591dcc44c81904da282ab5510ff38319d956
sebi-ai <sebastian.wimmer@whg-student.de>
2026-06-10 20:37:31 +0200

    Completed styling the widgets tab in the settings

--- script.js
+++ script.js
@@ -32,3 +32,40 @@ const textfeld = document.getElementById("location");
 checkbox.addEventListener("change", function() {
   textfeld.disabled = checkbox.checked;
 });
+
+const checkboxes = document.querySelectorAll('.limited-checkbox'); 
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', () => {
+    const checked = document.querySelectorAll('.limited-checkbox:checked');
+
+    if (checked.length > 3) { 
+      checkbox.checked = false;
+    }
+  });
+});
+
+const widgetCheckboxes = document.querySelectorAll('#widgets-list .limited-checkbox');
+
+function syncWidgetStars() {
+  widgetCheckboxes.forEach(widgetCheckbox => {
+    const widgetItem = widgetCheckbox.closest('li');
+    const starButton = widgetItem ? widgetItem.querySelector('.star-checkbox') : null;
+
+    if (!starButton) {
+      return;
+    }
+
+    starButton.disabled = !widgetCheckbox.checked;
+
+    if (!widgetCheckbox.checked) {
+      starButton.checked = false;
+    }
+  });
+}
+
+widgetCheckboxes.forEach(widgetCheckbox => {
+  widgetCheckbox.addEventListener('change', syncWidgetStars);
+});
+
+syncWidgetStars();
